0b723d01ccb6cce714f8d60e8b611c5258a299b6
Endrick <EPolFlo203@outlook.com>
2025-05-29 07:34:39 +0200

    FM-36 Fix: Added headers for requests

--- src/services/assignmentServiceApi.js
+++ src/services/assignmentServiceApi.js
@@ -1,12 +1,15 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
-export async function getAssignments(){
-    const response = await fetch(`${apiUrl}/assignment`,{
+export async function getAssignments() {
+    const response = await fetch(`${apiUrl}/assignment`, {
         method: 'GET',
-        
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'Content-Type': 'application/json'
+        }
     });
 
-    if (!response.ok){
+    if (!response.ok) {
         throw new Error('Error al recuperar los viajes');
     }
     return response.json();
